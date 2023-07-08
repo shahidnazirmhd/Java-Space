f2f9a2247bc391cd00eaf887370fac6a2eddf9cf
shahidnazirmhd <shahidnazirmhd@gmail.com>
2023-07-08 10:48:15 +0530

    08 Jul 23

---
+++ src/com/begin/examples/Exception1.java
@@ -0,0 +1,13 @@
+package com.begin.examples;
+
+public class Exception1 {
+    public static void main(String[] args) {
+        System.out.println(divide(5,0));
+    }
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by Zero");
+        }
+        return a/b;
+    }
+}
